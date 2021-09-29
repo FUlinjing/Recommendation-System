9383a9390adbb3744223d6f53517ee5ee736bafb
FUlinjing <60937317+FUlinjing@users.noreply.github.com>
2021-09-29 12:21:45 +0200

    Update app.py
    commit

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ def find_similar_movie(movie_name):
     if movieExists(movie_name,latent_matrix_1_mvp):
         a_1 = np.array(latent_matrix_1_mvp.loc[movie_name]).reshape(1, -1)
         score_1 = cosine_similarity(latent_matrix_1_mvp, a_1).reshape(-1)
-        dictDf = {'content': score_1} 
+        dictDf = {'content': score_1}
         similar = pd.DataFrame(dictDf, index = latent_matrix_1_mvp.index)
         similar.sort_values('content', ascending = False, inplace = True)
         return similar[1:].head(5)
     else:
-        raise ValueError('ERROR: The Movie is not Recognised')
+        return 'ERROR: The Movie is not Recognised'
 
 x=find_similar_movie(sequence)
 st.write(x)
